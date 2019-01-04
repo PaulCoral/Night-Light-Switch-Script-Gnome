8b667a48a4ef4bc93b5a1c75830f90fef67e0e24
Your Name <you@example.com>
2019-01-04 14:46:48 +0100

    print Author name

--- nightLight.sh
+++ nightLight.sh
@@ -4,6 +4,9 @@
 #Tested on Ubuntu 18.10
 #It could be useful to enable this script with a shortcut
 
+echo "NightLight Switch Script Gnome "
+echo "Author : Paul Coral"
+
 nightLight=$(gsettings get org.gnome.settings-daemon.plugins.color night-light-enabled)
 
 if [ "$nightLight" = true ] ;
